3319342dc9e84809610a424d201c3d35d8e1f568
Georgeo Nocera <georgeo.nocera@gmail.com>
2023-07-13 16:22:46 -0300

    inicial commit

---
+++ tmglobal/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# from django.shortcuts import render
+
+def home(request):
+    return render(request, 'tmglobal/pages/hlogin.html')
