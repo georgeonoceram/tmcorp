c4b0af8d781066393281e37ecb8b61f9bf9ca7ba
Georgeo Nocera <georgeo.nocera@gmail.com>
2023-07-24 17:40:48 -0300

    novo commit

--- tmglobal/forms.py
+++ tmglobal/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from django.contrib.auth import forms
 from .models import Users
 
+
 class UserChangeForms(forms.UserChangeForm):
     class Meta(forms.UserChangeForm.Meta):
         model = Users
+
 
 class UserCreationForms(forms.UserCreationForm):
     class Meta(forms.UserCreationForm.Meta):
